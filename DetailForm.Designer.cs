f715e9814004c8bba331a7c9ea57711ed78a0c50
leminhtriqs1996 <leminhtriqs1996@gmail.com>
2019-10-16 08:23:28 +0700

    1st

---
+++ DetailForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace QLSV
+{
+    partial class DetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textMSSV = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textKhoa = new System.Windows.Forms.TextBox();
+            this.textDHT = new System.Windows.Forms.TextBox();
+            this.dateTimeBirthDay = new System.Windows.Forms.DateTimePicker();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MSSV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Khoa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(377, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DHT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "BirthDay";
+            // 
+            // textMSSV
+            // 
+            this.textMSSV.Location = new System.Drawing.Point(106, 47);
+            this.textMSSV.Name = "textMSSV";
+            this.textMSSV.Size = new System.Drawing.Size(245, 20);
+            this.textMSSV.TabIndex = 5;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(106, 127);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(245, 20);
+            this.textName.TabIndex = 6;
+            // 
+            // textKhoa
+            // 
+            this.textKhoa.Location = new System.Drawing.Point(106, 192);
+            this.textKhoa.Name = "textKhoa";
+            this.textKhoa.Size = new System.Drawing.Size(182, 20);
+            this.textKhoa.TabIndex = 7;
+            // 
+            // textDHT
+            // 
+            this.textDHT.Location = new System.Drawing.Point(436, 189);
+            this.textDHT.Name = "textDHT";
+            this.textDHT.Size = new System.Drawing.Size(232, 20);
+            this.textDHT.TabIndex = 8;
+            // 
+            // dateTimeBirthDay
+            // 
+            this.dateTimeBirthDay.Location = new System.Drawing.Point(106, 254);
+            this.dateTimeBirthDay.Name = "dateTimeBirthDay";
+            this.dateTimeBirthDay.Size = new System.Drawing.Size(333, 20);
+            this.dateTimeBirthDay.TabIndex = 9;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(30, 27);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(48, 17);
+            this.radioButtonMale.TabIndex = 10;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(97, 27);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonFemale.TabIndex = 11;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonFemale);
+            this.groupBox1.Controls.Add(this.radioButtonMale);
+            this.groupBox1.Location = new System.Drawing.Point(426, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 67);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gioi Tinh";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.dateTimeBirthDay);
+            this.groupBox2.Controls.Add(this.textDHT);
+            this.groupBox2.Controls.Add(this.textKhoa);
+            this.groupBox2.Controls.Add(this.textName);
+            this.groupBox2.Controls.Add(this.textMSSV);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(33, 14);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(701, 325);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thong tin SV";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(139, 377);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(86, 41);
+            this.btnOK.TabIndex = 14;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(311, 369);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 49);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // DetailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "DetailForm";
+            this.Text = "DetailForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textMSSV;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textKhoa;
+        private System.Windows.Forms.TextBox textDHT;
+        private System.Windows.Forms.DateTimePicker dateTimeBirthDay;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ QLSV.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    public class QLSV
+    {
+        public delegate bool Compare(object o1, object o2);
+        public Compare p;
+        public SV[] listSV { get; set; }
+        public int count { get; set; }
+
+        public QLSV()
+        {
+            //ham tao ra cac doi tuong SV trong listSV
+            Add(new SV() { MSSV = "1", Name = "Rio Ferdinand", Khoa = "CNTT", Birthday = DateTime.Parse("12/16/1996"), DHT = 9.5, gender = true });
+            Add(new SV() { MSSV = "2", Name = "Nemamja Vidic", Khoa = "Marketing", Birthday = DateTime.Parse("12/26/1997"), DHT = 5.5, gender = false });
+            Add(new SV() { MSSV = "3", Name = "Chicharito", Khoa = "Du Lich", Birthday = DateTime.Parse("12/06/1998"), DHT = 6.5, gender = true });
+            Add(new SV() { MSSV = "4", Name = "Dimitar Berbatov", Khoa = "Ngon Ngu Anh", Birthday = DateTime.Parse("11/17/1999"), DHT = 9.9, gender = false });
+            Add(new SV() { MSSV = "5", Name = "Wayne Rooney", Khoa = "Kinh Te", Birthday = DateTime.Parse("02/28/1991"), DHT = 5.7, gender = true });
+            Add(new SV() { MSSV = "6", Name = "Edwin Van der Sar", Khoa = "Ngon Ngu Nhat", Birthday = DateTime.Parse("03/31/1992"), DHT = 3.6, gender = true });
+            Add(new SV() { MSSV = "8", Name = "Calos Tevez", Khoa = "CNTT", Birthday = DateTime.Parse("05/16/1993"), DHT = 10, gender = false });
+            Add(new SV() { MSSV = "9", Name = "Luis Nani", Khoa = "CNTT", Birthday = DateTime.Parse("01/16/2000"), DHT = 10, gender = true });
+            Add(new SV() { MSSV = "10", Name = "David Beckham", Khoa = "Du Lich", Birthday = DateTime.Parse("08/13/1993"), DHT = 10, gender = false });
+            Add(new SV() { MSSV = "11", Name = "David De Gea", Khoa = "Marketing", Birthday = DateTime.Parse("09/12/1992"), DHT = 10, gender = true });
+            Add(new SV() { MSSV = "12", Name = "Paul Pogba", Khoa = "Ngon Ngu Anh", Birthday = DateTime.Parse("05/26/2001"), DHT = 10, gender = false });
+            Add(new SV() { MSSV = "13", Name = "Joan Mata", Khoa = "Du Lich", Birthday = DateTime.Parse("09/16/1990"), DHT = 10, gender = true });
+            Add(new SV() { MSSV = "14", Name = "Patrice Evra", Khoa = "Ngon Ngu Nhat", Birthday = DateTime.Parse("07/16/1989"), DHT = 10, gender = false });
+            Add(new SV() { MSSV = "15", Name = "Michael Carrick", Khoa = "Ngon Ngu Nhat", Birthday = DateTime.Parse("05/16/1995"), DHT = 10, gender = true });
+            Add(new SV() { MSSV = "16", Name = "Ole Gunnar Solskjær", Khoa = "Marketing", Birthday = DateTime.Parse("05/16/1994"), DHT = 10, gender = false });
+        }
+
+        /*
+        public void CreateDB()
+        {
+            //ham tao ra cac doi tuong SV trong listSV
+            Add(new SV() { MSSV = "1", Name = "Nguyen Van A", Khoa = "CNTT", Birthday = DateTime.Parse("12/06/1996"), DHT = 9.5, gender = true });
+            Add(new SV() { MSSV = "2", Name = "Do Nam Trung", Khoa = "Marketing", Birthday = DateTime.Parse("12/06/1996"), DHT = 5.5, gender = false });
+            Add(new SV() { MSSV = "3", Name = "Tap Can Binh", Khoa = "Du Lich", Birthday = DateTime.Parse("12/06/1996"), DHT = 6.5, gender = true });
+            Add(new SV() { MSSV = "4", Name = "Kim Jong un", Khoa = "Ngon Ngu Anh", Birthday = DateTime.Parse("11/06/1996"), DHT = 9.9, gender = false });
+            Add(new SV() { MSSV = "5", Name = "Iron Man", Khoa = "Kinh Te", Birthday = DateTime.Parse("12/06/1996"), DHT = 5.7, gender = true });
+            Add(new SV() { MSSV = "6", Name = "Lionel Messi", Khoa = "Ngon Ngu Nhat", Birthday = DateTime.Parse("12/06/1996"), DHT = 3.6, gender = true });
+            Add(new SV() { MSSV = "7", Name = "Marcus Rashfosh", Khoa = "CNTT", Birthday = DateTime.Parse("12/06/1996"), DHT = 10, gender = false });
+
+        }
+        */
+        public void Add(SV s)
+        {
+            // add SV vao cuoi listSV
+            if (this.count == 0)
+            {
+                this.listSV = new SV[this.count + 1];
+                this.listSV[this.count] = s;
+            } 
+            else
+            {
+                SV[] temp = new SV[this.count];
+                temp = this.listSV;
+                this.listSV = new SV[this.count + 1];
+                for ( int i = 0; i < this.count; i++)
+                {
+                    this.listSV[i] = temp[i];
+                }
+                this.listSV[this.count] = s;
+            }
+            this.count++;
+        }
+        public string[] GetListNameKhoa()
+        {
+            // lay cac string Khoa khong trung nhau va luu vao mang string
+            string[] listNameKhoa = new string[this.count]; 
+            for (int i = 0; i < this.count; i++)
+            {
+                listNameKhoa[i] = this.listSV[i].Khoa;
+            }
+            return listNameKhoa.Distinct().ToArray();
+        }
+        public SV[] GetListSVByKhoa(string k)
+        {
+            // lay cac doi tuong sinh vien co thuoc tinh Khoa = k , tra ve 1 mang
+            
+            if (k.Equals("ALL"))
+                return this.listSV;
+            else
+            {
+                SV[] listSVByKhoa = null;
+                int count = 0;
+                foreach (SV s in this.listSV)
+                {
+                    if (s.Khoa.Equals(k))
+                    {
+                        if (count == 0)
+                        {
+                            listSVByKhoa = new SV[count + 1];
+                            listSVByKhoa[count] = s;
+                        }
+                        else
+                        {
+                            SV[] temp = new SV[count];
+                            temp = listSVByKhoa;
+                            listSVByKhoa = new SV[count + 1];
+                            for (int i = 0; i < count; i++)
+                            {
+                                listSVByKhoa[i] = temp[i];
+                            }
+                            listSVByKhoa[count] = s;
+                        }
+                        count++;
+                    }
+                }
+                return listSVByKhoa;
+            }
+            
+        }
+        public void RemoveAt(int index)
+        {
+            // xoa doi tuong SV trong listSV tai vi tri index
+            if (index == 0 )
+            {
+                SV[] temp = new SV[this.count - 1];
+                for (int i = 0; i < this.count -1 ; i++)
+                {
+                    temp[i] = this.listSV[i + 1];
+                }
+                this.listSV = temp;
+            }
+            else if (index == count-1)
+            {
+                SV[] temp = new SV[this.count];
+                temp = this.listSV;
+                this.listSV = new SV[this.count - 1];
+                for (int i = 0; i < this.count - 1; i++)
+                {
+                    this.listSV[i] = temp[i];
+                }
+            } else
+            {
+                SV[] temp = new SV[this.count];
+                temp = this.listSV;
+                this.listSV = new SV[this.count - 1];
+                for (int i=0,j=0; i < this.count ; i++ ,j++)
+                {
+                    if ( i == index )
+                    {
+                        j--;
+                        continue;
+                    }
+                    this.listSV[j] = temp[i];
+                }
+            }
+            count--;
+        }
+        public void DelSV(string m)
+        {
+            // tim trong listSV co doi tuong SV nao ma MSSV = m -> tra ve vi tri tim duoc
+            int index = -1;
+            for (int i = 0; i< this.count; i ++)
+            {
+                if ((this.listSV[i]).MSSV.Equals(m))
+                    index = i;
+            }
+            // Xoa doi tuong do di su dung RemoveAt()
+            if (index != -1)
+                RemoveAt(index);
+        }
+        public SV[] GetListSVByName(string name)
+        {
+            SV[] listSVByName = null;
+            int count = 0;
+            // tim kiem cac doi tuong SV trong listSV theo name
+            foreach(SV s in this.listSV)
+            {
+                if (s.Name.Contains(name))
+                {
+                    if (count == 0)
+                    {
+                        listSVByName = new SV[count + 1];
+                        listSVByName[count] = s;
+                    }
+                    else
+                    {
+                        SV[] temp = new SV[count];
+                        temp = listSVByName;
+                        listSVByName = new SV[count + 1];
+                        for (int i = 0; i < count; i++)
+                        {
+                            listSVByName[i] = temp[i];
+                        }
+                        listSVByName[count] = s;
+                    }
+                    count++;
+                }
+            }
+            return listSVByName;
+        }
+        public void Sort(string khoa)
+        {
+            
+            if (khoa.Equals("MSSV"))
+            {
+                p = new Compare(SV.CompareMSSV);
+            }
+            if (khoa.Equals("Name"))
+            {
+                p = new Compare(SV.CompareName);
+            }
+            if (khoa.Equals("Khoa"))
+            {
+                p = new Compare(SV.CompareKhoa);
+            }
+
+            for (int i=0; i< count -1  ; i++)
+            {
+                for (int j=i+1; j< count; j++)
+                {
+                   if ( p(this.listSV[i],this.listSV[j]) )
+                    {
+                        SV temp = this.listSV[i];
+                        this.listSV[i] = this.listSV[j];
+                        this.listSV[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public SV GetSV(string m)
+        {
+            // tim kiem, tra ve doi tuong SV co MSSV = m
+            SV sv = new SV();
+            foreach (SV s in this.listSV)
+            {
+                if (s.MSSV.Equals(m))
+                {
+                    sv = s;
+                    break;
+                }
+            }
+            return sv;
+        }
+        public int IndexOf(SV s)
+        {
+            //tra ve vi tri cua SV s
+            int index = -1;
+            for (int i=0; i< this.count; i++)
+            {
+                if (this.listSV[i].Equals(s))
+                    index = i;
+            }
+            return index;
+        }
+    }
+}
